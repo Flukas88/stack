b989c326843a45fae12bb7fe8971a8e413ec2e5e
Luca Francesca <luca@lucafrancesca.me>
2017-07-12 19:27:08 +0100

    Some cleaning
    Signed-off-by: Luca Francesca <luca.francesca01@gmail.com>

--- main.c
+++ main.c
@@ -5,16 +5,18 @@
 
 
 int main() {
-    char first_element[] = "String0";
+    char first_element[] = "String00";
     node *head;
     head = push(NULL, first_element);
-    printList(head);
+    printf("Pushing in %d elements\n", MAX_EL-1);
+    printf("Peak at step 0: %s\n", peak(head));
+    //printList(head);
     char text[] = "String01";
     for (uint8_t i = 1; i < MAX_EL - 1 ; i++) {
         text[6] = (char) ((i / 10) + '0');
         text[7] = (char) ((i % 10) + '0');
         push(head, text);
-        printList(head);
+        //printList(head);
     }
 #ifdef DEBUG
     printf("\nStarts with pop(ing)\n\n");
@@ -23,8 +25,8 @@ int main() {
     n = calloc(STRING_SIZE, sizeof(char));
     for (uint8_t i = 1; i < MAX_EL-1; i++) {
         strncpy(n, pop(&head),1);
-        printList(head);
-        printf("Peak: %s\n", peak(head));
+        //printList(head);
+        printf("Peak at step %d: %s\n", i, peak(head));
     }
 #ifdef DEBUG
     printf("\nStarts freeing \n\n");
