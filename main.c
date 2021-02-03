632fbea4ff6a05ba21f68f4d1dc19972bef3eeda
Luca Francesca <lfrancesca@salesforce.com>
2021-02-03 09:02:59 +0000

    Debug printing
    Moved debug printing under a ifdef

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include "list.h"
 
-
 int main(int argc, char **argv) {
 
     if (argc < 2) {
@@ -13,7 +12,9 @@ int main(int argc, char **argv) {
     token = strtok(argv[1], " ");
 
     while( token != NULL ) {
+        #ifdef DEBUG
         printf("Looking at %s\n", token);
+        #endif
         if (is_valid_int(token) == true) {
             push(atoi(token));
         } else {
@@ -21,28 +22,36 @@ int main(int argc, char **argv) {
                 op1 = pop();
                 op2 = pop();
                 push(op1 + op2);
-                printf("Pushing %f + %f = %f\n", op1, op2, (op1 + op2));
-
+                #ifdef DEBUG
+                printf("Pushing %f + %f = %f\n", op2, op1, (op1 + op2));
+                #endif
             } else if (strstr(token, "-") != NULL) {
                 op1 = pop();
                 op2 = pop();
                 push(op2 - op1);
-                printf("Pushing %f - %f = %f\n", op1, op2, (op2 - op1));
-
+                #ifdef DEBUG
+                printf("Pushing %f - %f = %f\n", op2, op1, (op2 - op1));
+                #endif
             } else if (strstr(token, "x") != NULL) {
                 op1 = pop();
                 op2 = pop();
                 push(op1 * op2);
-                printf("Pushing %f x %f = %f\n", op1, op2, (op1 * op2));
+                #ifdef DEBUG
+                printf("Pushing %f x %f = %f\n", op2, op1, (op1 * op2));
+                #endif
             } else if (strstr(token, "*") != NULL) {
                 op1 = pop();
                 op2 = pop();
                 push(op1 * op2);
-                printf("Pushing %f * %f = %f\n", op1, op2, (op1 * op2));
+                #ifdef DEBUG
+                printf("Pushing %f * %f = %f\n", op2, op1, (op1 * op2));
+                #endif
             }  else if (strstr(token, "/") != NULL) {
                 op1 = pop();
                 op2 = pop();
-                printf("Pushing %f / %f = %f\n", op1, op2, (op2 / op1));
+                #ifdef DEBUG
+                printf("Pushing %f / %f = %f\n", op2, op1, (op2 / op1));
+                #endif
                 if ((op1 == 0.0) || (op2 == 0.0) ) { printf("Division by 0 is not possible! \n"); } else {
                     push((op2 / op1));
                 }
